92908f4cf713eaf025c7346b4681c8293bd6f6db
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2024-04-13 15:41:45 +0300

    some request implementation

--- internal/infrastructure/yandexLanguageModel/request_body.go
+++ internal/infrastructure/yandexLanguageModel/request_body.go
@@ -1,6 +1,22 @@
 package yandex_language_model
 
+import "fmt"
+
 type RequestBody struct {
 	modelUri          string            `json:"modelUri"`
 	completionOptions CompletionOptions `json:"completionOptions"`
+	messages          []Message         `json:"messages"`
+}
+
+func getModelUri(token string) string {
+	return fmt.Sprintf("gpt://%s/yandexgpt-lite", token)
+}
+
+func NewRequestBody(token string, completionOptions CompletionOptions, messages []Message) RequestBody {
+	modelUri := getModelUri(token)
+	return RequestBody{
+		modelUri:          modelUri,
+		completionOptions: completionOptions,
+		messages:          messages,
+	}
 }
